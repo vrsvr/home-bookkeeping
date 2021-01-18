96f449a4b93caa2dad5db3f55b7ae3e52a6d09a6
vrsvr <rifatvarisov@gmail.com>
2021-01-18 14:50:08 +0500

    Редактирование категорий затрат

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,14 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
+      <Loader v-if="loading" />
+      <div class="row" v-else>
+
         <CategoryCreate @created="addNewCategory"/>
-        <CategoryEdit />
+
+        <CategoryEdit v-if="categories.length" :categories="categories" :key="categories.length + updateCount" @updated="updateCategories"/>
+
+        <p v-else class="center">Создайте категорию</p>
       </div>
     </section>
   </div>
@@ -19,15 +24,26 @@ import CategoryEdit from '@/components/CategoryEdit'
 export default {
   name: 'categories',
   data: () => ({
-    categories: []
+    categories: [],
+    loading: true,
+    updateCount: 0
   }),
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+  },
   components: {
     CategoryCreate, CategoryEdit
   },
   methods: {
     addNewCategory(category) {
       this.categories.push(category)
-      console.log(this.categories)
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex(c => c.id === category.id)
+      this.categories[idx].title = category.title
+      this.categories[idx].limit = category.limit
+      this.updateCount++
     }
   }
 }
