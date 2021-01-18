d7ae9d8638e0b97d69fbd6941b0a42347cee26be
vrsvr <rifatvarisov@gmail.com>
2021-01-18 17:55:02 +0500

    Страница планирования

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
 import messagePlugin from '@/utils/message.plugin'
+import tooltipDirective from '@/directives/tooltip.directive'
 import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
-
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
@@ -20,6 +20,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 
 firebase.initializeApp({
