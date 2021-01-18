773892b93e54932cd9c752caf5a0d95132b989c6
vrsvr <rifatvarisov@gmail.com>
2021-01-18 12:59:02 +0500

    Главная страница (счет и курсы валют)

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import dateFilter from '@/filters/date.filters'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
 import messagePlugin from '@/utils/message.plugin'
+import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -17,6 +19,8 @@ Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 firebase.initializeApp({
   apiKey: "AIzaSyA0-_4ejpGlMvYR3Vm3lXik2FOCrlV3fQA",
